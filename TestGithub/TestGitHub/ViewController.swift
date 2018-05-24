df4cb569c2ab1298c92d348bdd078fa46c374828
Andrey <andrey.dovzhenko.ios@gmail.com>
2018-05-24 16:35:24 +0300

    * branch2

--- TestGithub/TestGitHub/ViewController.swift
+++ TestGithub/TestGitHub/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-          print("Branch1")
+          print("Branch2")
     }
 
     override func didReceiveMemoryWarning() {
