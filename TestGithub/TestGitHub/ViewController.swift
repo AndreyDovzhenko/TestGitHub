9b619d58ffe258e2fe07434863d438fe57759a6f
Andrey <andrey.dovzhenko.ios@gmail.com>
2018-05-24 16:21:45 +0300

    * branch1

--- TestGithub/TestGitHub/ViewController.swift
+++ TestGithub/TestGitHub/ViewController.swift
@@ -12,15 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        print("Branch1")
+          print("Branch1")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-        print("Branch2")
+      
     }
 
 
