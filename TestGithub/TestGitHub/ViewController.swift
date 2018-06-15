1616de898daf7e9cd6ba88dd9a96f21a6bd1894e
Andrey <andrey.dovzhenko.ios@gmail.com>
2018-06-15 12:16:00 +0300

    new commit

--- TestGithub/TestGitHub/ViewController.swift
+++ TestGithub/TestGitHub/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Branch")
+        print("Hello")
     }
 
     override func didReceiveMemoryWarning() {
